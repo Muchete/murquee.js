499f5391c632a73a6ae0cac562a9993be076a1a7
Michael Schönenberger <muchi94@gmail.com>
2018-09-15 10:38:52 +0200

    Update murquee.js

--- murquee.js
+++ murquee.js
@@ -1,10 +1,9 @@
 var cW = $(".mq-container").width();
 var t = $(".mq-title").outerWidth(true);
 var tW;
-var step = 0.05;
+var step = -0.05;
 var pos;
 var percentOfParent;
-var dir = -1;
 
 //init here
 // create duplicates
@@ -17,14 +16,8 @@ function init() {
       .clone();
     tElem.appendTo($(".mq-title-wrap"));
     tW = $(".mq-title-wrap").outerWidth();
-  } while (cW + t * 4 >= tW);
+  } while (cW + t * 3 >= tW);
   tW = $(".mq-title-wrap").outerWidth();
-}
-
-function setT(){
-  var d = $(".mq-title").eq(1).position().left;
-  console.log(d);
-
 }
 //--------
 
@@ -41,33 +34,22 @@ function resetLeft() {
 function setLeft(l) {
   $(".mq-title-wrap").css("left", l + "%");
 }
-//$(".mq-title-wrap").css('animation','mq-scroll-'+ direction +' '+speed+'s infinite linear');
 
 function animate() {
   var id = setInterval(frame, 1);
 }
 
 function frame() {
-  if (dir > 0) {
-    if (pos < percentOfParent) {
-      pos+=step;
-    } else {
+    if (step > 0 && pos > percentOfParent) {
       pos = pos+percentOfParent;
-    }
-    setLeft(pos);
-  } else {
-    console.log(pos);
-    if (pos > percentOfParent) {
-      pos-=step;
-    } else {
+    } else if (step < 0 && pos < percentOfParent*2){
       pos = pos-percentOfParent;
+    } else if (step != 0) {
+      pos+=step;
     }
     setLeft(pos);
-  }
 }
 
-
 init();
-setT();
 setToOffset();
 animate();
